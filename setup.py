15eebea7cbbf8202a11a6b1f8f146fd3e6f3f346
Fabien Bochu <fabien.bochu@polyconseil.fr>
2018-01-05 17:05:52 +0100

    Switch to setup.cfg for package metadata

--- setup.py
+++ setup.py
@@ -1,56 +1,10 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
-
-
-def read(file_path):
-    with open(file_path) as fp:
-        return fp.read()
+#! /usr/bin/env python
+from setuptools import setup
 
 
 setup(
-    name='curtis',
-    version='0.2.dev0',
-    description="A simple sentry cli",
-    long_description=read('README.rst'),
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3 :: Only',
-        'Topic :: Software Development :: Bug Tracking',
-        'Topic :: System :: Logging',
-        'Topic :: Terminals',
-        'Topic :: Utilities',
-    ],
-    keywords=['sentry', 'cli'],
-    author='Polyconseil',
-    author_email='opensource+curtis@polyconseil.fr',
-    url='https://github.com/Polyconseil/curtis/',
-    license='BSD',
-    packages=find_packages(where='src'),
-    package_dir={'': str('src')},
-    include_package_data=True,
-    zip_safe=False,
-    setup_requires=[
-        'setuptools',
-    ],
-    install_requires=[
-        'click',
-        'requests',
-        'termcolor',
-    ],
-    entry_points={
-        'console_scripts': (
-            'curtis = curtis.__main__:main',
-        ),
+    setup_requires=['setuptools>=30.3'],
+    package_dir={  # FIXME: wait for https://github.com/pypa/setuptools/issues/1136
+        '': 'src',
     },
 )
